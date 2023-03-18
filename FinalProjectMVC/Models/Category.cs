57caf7cf9c444f55d659dbe68c27513ac26b528c
RaoufAlaadin <RaoufAlaadin97@gmail.com>
2023-03-18 19:09:44 +0200

    firstcommitraouf

---
+++ FinalProjectMVC/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalProjectMVC.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        // Navigation property for Products
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
