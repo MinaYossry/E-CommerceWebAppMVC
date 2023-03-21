223c08c7f373d54ed5e44bb1464a6b0ab79478e1
Mina Yossry <mina.yossry4@gmail.com>
2023-03-21 08:55:17 +0200

    fix

--- FinalProjectMVC/Areas/Identity/Data/DbSeeder.cs
+++ FinalProjectMVC/Areas/Identity/Data/DbSeeder.cs
@@ -1,5 +1,6 @@
 ﻿using FinalProjectMVC.Constants;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
 using System;
 
 namespace FinalProjectMVC.Areas.Identity.Data
@@ -15,42 +16,45 @@ namespace FinalProjectMVC.Areas.Identity.Data
             var roleManager = service.GetService<RoleManager<IdentityRole>>();
 
             // 2- creating and adding roles to the `roleManger` Instance
-            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
-            await roleManager.CreateAsync(new IdentityRole(Roles.Seller.ToString()));
-            await roleManager.CreateAsync(new IdentityRole(Roles.Customer.ToString()));
-            await roleManager.CreateAsync(new IdentityRole(Roles.CustomerService.ToString()));
-            await roleManager.CreateAsync(new IdentityRole(Roles.WebDeveloper.ToString()));
+            if (roleManager is not null && userManager is not null )
+            {
+                await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+                await roleManager.CreateAsync(new IdentityRole(Roles.Seller.ToString()));
+                await roleManager.CreateAsync(new IdentityRole(Roles.Customer.ToString()));
+                await roleManager.CreateAsync(new IdentityRole(Roles.CustomerService.ToString()));
+                await roleManager.CreateAsync(new IdentityRole(Roles.WebDeveloper.ToString()));
 
 
             // creating admin in code to be secure. 
 
-            var user = new ApplicationUser
-            {
-                UserName = "admin@gmail.com",
-                Email = "admin@gmail.com",
-                Name = "Raoufalaadin",
-                EmailConfirmed = true,
-                PhoneNumberConfirmed = true
-            };
-            var userInDb = await userManager.FindByEmailAsync(user.Email);
+                var user = new ApplicationUser
+                {
+                    UserName = "admin@gmail.com",
+                    Email = "admin@gmail.com",
+                    Name = "Raoufalaadin",
+                    EmailConfirmed = true,
+                    PhoneNumberConfirmed = true
+                };
+                var userInDb = await userManager.FindByEmailAsync(user.Email);
 
 
-            // If no user is found with that email (Email = "admin@gmail.com")
-            // Then we create an new account and set it as admin.
+                // If no user is found with that email (Email = "admin@gmail.com")
+                // Then we create an new account and set it as admin.
 
-            // This part ensures that there is always an admin account.
-            if (userInDb == null)
-            {
-                // setting it's password
-                await userManager.CreateAsync(user, "Admin@123");
-                // setting it's role
-                await userManager.AddToRoleAsync(user, Roles.Admin.ToString());
-            }
+                // This part ensures that there is always an admin account.
+                if (userInDb == null)
+                {
+                    // setting it's password
+                    await userManager.CreateAsync(user, "Admin@123");
+                    // setting it's role
+                    await userManager.AddToRoleAsync(user, Roles.Admin.ToString());
+                }
 
-            /* If the email does exist, we will assing the role in the 
-             register.cs for some reason ====> search !! 
+                /* If the email does exist, we will assing the role in the 
+                 register.cs for some reason ====> search !! 
             
-             */
+                 */
+            }
         }
 
     }
