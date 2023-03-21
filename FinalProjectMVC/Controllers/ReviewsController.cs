7487764c4917205bea485f905bc7dcefb9cd0ada
lenovo <lenovo@DESKTOP-P34L1PR>
2023-03-21 21:44:55 +0200

    Add Review

---
+++ FinalProjectMVC/Controllers/ReviewsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FinalProjectMVC.Models;
+
+namespace FinalProjectMVC.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private readonly StoreDbContext _context;
+
+        public ReviewsController(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reviews
+        public async Task<IActionResult> Index()
+        {
+
+            var storeDbContext = _context.Reviews.Include(r => r.Customer).Include(r => r.Product);
+            return View(await storeDbContext.ToListAsync());
+        }
+
+        // GET: Reviews/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    //  Get Product With Reviews
+        //    if (id == null || _context.Reviews == null || _context.Reviews.FirstOrDefaultAsync(x => x.ProductId == id) == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var review = await _context.Reviews
+        //        .Include(r => r.Product)
+        //        .FirstOrDefaultAsync(m => m.ProductId == id);
+        //    if (review == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(review);
+        //}
+
+        // GET: Reviews/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Email");
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Name");
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Rating,CustomerId,ProductId")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(review);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Email", review.CustomerId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Name", review.ProductId);
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Reviews == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Email", review.CustomerId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Name", review.ProductId);
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Rating,CustomerId,ProductId")] Review review)
+        {
+            if (id != review.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewExists(review.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Email", review.CustomerId);
+            ViewData["ProductId"] = new SelectList(_context.Products, "Id", "Name", review.ProductId);
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Reviews == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .Include(r => r.Customer)
+                .Include(r => r.Product)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Reviews == null)
+            {
+                return Problem("Entity set 'StoreDbContext.Reviews'  is null.");
+            }
+            var review = await _context.Reviews.FindAsync(id);
+            if (review != null)
+            {
+                _context.Reviews.Remove(review);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewExists(int id)
+        {
+          return (_context.Reviews?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
