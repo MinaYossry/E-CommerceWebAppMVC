a2ad6d66316cabfc80b72c4a4e7fbf34015fecb4
Mina Yossry <mina.yossry4@gmail.com>
2023-03-23 18:55:23 +0200

    Modify Users

--- FinalProjectMVC/Areas/AdminPanel/Models/Admin.cs
+++ FinalProjectMVC/Areas/AdminPanel/Models/Admin.cs
@@ -1,8 +1,13 @@
-﻿namespace FinalProjectMVC.Areas.AdminPanel.Models
+﻿using FinalProjectMVC.Areas.Identity.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinalProjectMVC.Areas.AdminPanel.Models
 {
     public class Admin
     {
+        [ForeignKey("ApplicationUser")]
+        public required string Id { get; set; }
 
-
+        public virtual ApplicationUser? ApplicationUser { get; set; }
     }
 }
--- FinalProjectMVC/Areas/SellerPanel/Models/Seller.cs
+++ FinalProjectMVC/Areas/SellerPanel/Models/Seller.cs
@@ -1,17 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using FinalProjectMVC.Areas.Identity.Data;
 using FinalProjectMVC.Models;
 
 namespace FinalProjectMVC.Areas.SellerPanel.Models
 {
-    public class Seller : Person
+    public class Seller
     {
-        public decimal Balance { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public required string Id { get; set; }
+
+        public decimal Balance { get; set; } = 0;
 
         public string? TaxNumber { get; set; }
 
-        [Range(1, 5)]
-        public int Rating { get; set; }
+
+        public virtual ApplicationUser? ApplicationUser { get; set; }
 
         public virtual ICollection<SellerProduct>? SellerProducts { get; set; }
+        public virtual List<Address>? Addresses { get; set; }
     }
 }
