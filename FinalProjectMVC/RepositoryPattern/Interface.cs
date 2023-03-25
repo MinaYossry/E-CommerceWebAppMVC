431813a94f6474ab1480de4b640092764d29b6ea
Mina Yossry <mina.yossry4@gmail.com>
2023-03-25 17:56:37 +0200

    Add Async version for all methods

--- FinalProjectMVC/RepositoryPattern/Interface.cs
+++ FinalProjectMVC/RepositoryPattern/Interface.cs
@@ -9,5 +9,12 @@
         public void Update<PType>(PType id, T Entity);
         public void Delete<PType>(PType id);
         public List<T> Filter(Func<T, bool> filterPredicate);
+
+        public Task<List<T>> GetAllAsync();
+        public Task<T?> GetDetailsAsync<PType>(PType id);
+        public Task InsertAsync(T Entity);
+        public Task UpdateAsync<PType>(PType id, T Entity);
+        public Task DeleteAsync<PType>(PType id);
+        public Task<List<T>> FilterAsync(Func<T, bool> filterPredicate);
     }
 }
