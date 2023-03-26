1fc116d8dd262dd926b1d6571ede8094a8a9b1a8
Mina Yossry <mina.yossry4@gmail.com>
2023-03-26 21:33:09 +0200

    removing context injection

--- FinalProjectMVC/Areas/AdminPanel/Controllers/ReportsController.cs
+++ FinalProjectMVC/Areas/AdminPanel/Controllers/ReportsController.cs
@@ -17,12 +17,15 @@ namespace FinalProjectMVC.Areas.AdminPanel.Controllers
     [Area("AdminPanel")]
     public class ReportsController : Controller
     {
-        private readonly ApplicationDbContext _context;
+       // private readonly ApplicationDbContext _context;
         private readonly IRepository<Report> reportRepo;
 
-        public ReportsController(ApplicationDbContext context, IRepository<Report> reportRepo)
+        public ReportsController(
+            //ApplicationDbContext context,
+            IRepository<Report> reportRepo
+            )
         {
-            _context = context;
+        //    _context = context;
             this.reportRepo = reportRepo;
         }
 
@@ -57,63 +60,6 @@ namespace FinalProjectMVC.Areas.AdminPanel.Controllers
 
 
             return View(viewMode);
-        }
-
-        // GET: AdminPanel/Reports/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Reports == null)
-            {
-                return NotFound();
-            }
-
-            var report = await _context.Reports
-                .Include(r => r.Review)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (report == null)
-            {
-                return NotFound();
-            }
-
-            return View(report);
-        }
-
-        // GET: AdminPanel/Reports/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Reports == null)
-            {
-                return NotFound();
-            }
-
-            var report = await _context.Reports
-                .Include(r => r.Review)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (report == null)
-            {
-                return NotFound();
-            }
-
-            return View(report);
-        }
-
-        // POST: AdminPanel/Reports/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Reports == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Reports'  is null.");
-            }
-            var report = await _context.Reports.FindAsync(id);
-            if (report != null)
-            {
-                _context.Reports.Remove(report);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         [HttpPost]
@@ -152,9 +98,68 @@ namespace FinalProjectMVC.Areas.AdminPanel.Controllers
             return RedirectToAction("Index");
         }
 
-        private bool ReportExists(int id)
-        {
-          return (_context.Reports?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+        //// GET: AdminPanel/Reports/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null || _context.Reports == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var report = await _context.Reports
+        //        .Include(r => r.Review)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (report == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(report);
+        //}
+
+        //// GET: AdminPanel/Reports/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null || _context.Reports == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var report = await _context.Reports
+        //        .Include(r => r.Review)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (report == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(report);
+        //}
+
+        //// POST: AdminPanel/Reports/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    if (_context.Reports == null)
+        //    {
+        //        return Problem("Entity set 'ApplicationDbContext.Reports'  is null.");
+        //    }
+        //    var report = await _context.Reports.FindAsync(id);
+        //    if (report != null)
+        //    {
+        //        _context.Reports.Remove(report);
+        //    }
+
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+
+
+        //private bool ReportExists(int id)
+        //{
+        //  return (_context.Reports?.Any(e => e.Id == id)).GetValueOrDefault();
+        //}
     }
 }
