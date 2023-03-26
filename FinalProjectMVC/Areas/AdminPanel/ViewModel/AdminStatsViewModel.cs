e568147c57fa713465f8ce96267f5992a74b62f3
Mina Yossry <mina.yossry4@gmail.com>
2023-03-26 20:04:44 +0200

    Revert "Revert "update Admin front page""
    This reverts commit 158ac6a60024c231cec8aff670b78968c5a017d1.

---
+++ FinalProjectMVC/Areas/AdminPanel/ViewModel/AdminStatsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FinalProjectMVC.Areas.AdminPanel.ViewModel
+{
+    public class AdminStatsViewModel
+    {
+        public int NewOrders { get; set; }
+
+        public int NewProducts { get; set; }
+
+        public int Sellers { get; set; }
+
+        public int Customers { get; set; }
+
+    }
+}
