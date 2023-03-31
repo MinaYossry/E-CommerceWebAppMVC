48647db6449e480b3a6e11e51666f7d01c0136a2
Mina Yossry <mina.yossry4@gmail.com>
2023-03-31 22:19:19 +0200

    update errors

--- FinalProjectMVC/Controllers/HomeController.cs
+++ FinalProjectMVC/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using FinalProjectMVC.Areas.Identity.Data;
 using FinalProjectMVC.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -29,10 +30,39 @@ namespace FinalProjectMVC.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [Route("Home/Error")]
+        public IActionResult Error(int? statusCode = null)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
+
+            string errorMessage;
+            switch (statusCode)
+            {
+                case 400:
+                    errorMessage = "Bad Request";
+                    break;
+                case 401:
+                    errorMessage = "Unauthorized";
+                    break;
+                case 403:
+                    errorMessage = "Forbidden";
+                    break;
+                case 404:
+                    errorMessage = "Page Not Found";
+                    break;
+                case 500:
+                    errorMessage = "Internal Server Error";
+                    break;
+                default:
+                    var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+                    var exception = exceptionHandlerPathFeature?.Error;
+                    errorMessage = exception?.Message;
+                    break;
+            }
+            ViewData["StatusCode"] = statusCode;
+            ViewData["ErrorMessage"] = errorMessage;
+            return View();
+
         }
     }
 }
--- FinalProjectMVC/Program.cs
+++ FinalProjectMVC/Program.cs
@@ -107,16 +107,20 @@ builder.Services.AddScoped<IRepository<SubCategory>, SubCategoryRepoService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseMigrationsEndPoint();
+//}
+//else
+//{
+//    app.UseExceptionHandler("/Home/Error");
+//    app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+app.UseExceptionHandler("/Home/Error");
+app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
