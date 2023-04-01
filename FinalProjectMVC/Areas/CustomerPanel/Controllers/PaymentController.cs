fe89063a52dbb2ccea0a4b03340f0c5308c9b3c4
Mina Yossry <mina.yossry4@gmail.com>
2023-04-01 20:55:30 +0200

    fix bug

--- FinalProjectMVC/Areas/CustomerPanel/Controllers/PaymentController.cs
+++ FinalProjectMVC/Areas/CustomerPanel/Controllers/PaymentController.cs
@@ -43,7 +43,7 @@ namespace FinalProjectMVC.Areas.CustomerPanel.Controllers
             List<OrderItem> My_Order_Items = new List<OrderItem>();
 
 
-            var domain = "https://localhost:5009/";
+            var domain = "https://localhost:7111/";
             var options = new SessionCreateOptions
             {
                 LineItems = new List<SessionLineItemOptions>(),
